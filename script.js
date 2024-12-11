f69d81852f47ff1a0ea01566bfcbafaa3a1a6551
Eagle3343 <Eagle3343>
2024-12-11 18:06:29 +0400

    code added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+function mostExpensive(list){
+
+    let expensive =0;
+    for(const item of list){
+       expensive=item.price ;
+      
+       }
+       return expensive
+    }
+   
+   let result =mostExpensive([
+    {product :"Diamond Earrings",price:980},
+     {product:"Gold Watch",price:250},
+     {product:"Pearl Necklace", price:4650},
+   ])
+   
+   console.log(result);
+   
+   
+   
